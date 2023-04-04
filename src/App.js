3c7021efc391bf946f4850a1dafbf1d0ece67147
devashish gupta <89517503+Itsdevashishgupta@users.noreply.github.com>
2023-04-04 13:54:48 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-const form = document.querySelector('#form');
-const screenshot = document.querySelector('#screenshot');
+// Get references to the form inputs and screenshot element
+const form = document.querySelector('form');
+const urlInput = document.querySelector('#url');
+const widthInput = document.querySelector('#width');
+const heightInput = document.querySelector('#height');
+const formatInput = document.querySelector('#format');
+const qualityInput = document.querySelector('#quality');
+const screenshotEl = document.querySelector('#screenshot');
+const YOUR_API_KEY = 'your_api_key_here';
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
+// Listen for form submit event
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
 
-  const url = document.querySelector('#url').value;
-  const apiKey = 'YOUR_API_KEY';
-  const apiUrl = `https://www.exponentialhost.com/api/website-screenshot/?url=${url}&key=${apiKey}`;
+  // Get the form input values
+  const url = urlInput.value;
+  const width = widthInput.value;
+  const height = heightInput.value;
+  const format = formatInput.value;
+  const quality = qualityInput.value;
 
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    if (data.success) {
-      screenshot.innerHTML = `<img src="${data.url}" alt="Screenshot of ${url}">`;
-    } else {
-      screenshot.innerHTML = 'Error: ' + data.message;
-    }
-  } catch (error) {
-    screenshot.innerHTML = 'Error: ' + error.message;
+  // Show the loading spinner
+  screenshotEl.classList.add('loading');
+
+  // Call the API to generate the screenshot
+  const response = await fetch(`https://api.apiflash.com/v1/urlto${format}?access_key=${YOUR_API_KEY}&url=${url}&height=${height}&width=${width}&quality=${quality}`);
+
+  // If the response is successful, show the screenshot image
+  if (response.ok) {
+    const blob = await response.blob();
+    const objectURL = URL.createObjectURL(blob);
+    const img = new Image();
+    img.onload = () => {
+      screenshotEl.classList.remove('loading');
+      screenshotEl.querySelector('img').setAttribute('src', objectURL);
+      screenshotEl.querySelector('img').style.display = 'block';
+    };
+    img.src = objectURL;
+  } else {
+    // If the response is not successful, show an error message
+    screenshotEl.classList.remove('loading');
+    screenshotEl.innerHTML = `<p>Error generating screenshot. Please try again later.</p>`;
   }
 });
